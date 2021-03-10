cfc94319897601ffc144df8ae501acc8e5fe43a3
Tomasz Bartosiewicz <2415677+tombart@users.noreply.github.com>
2021-03-10 13:58:02 +0000

    PPT-531: skeleton for ppt returns frontend

---
+++ project/AppDependencies.scala
@@ -0,0 +1,30 @@
+import play.core.PlayVersion.current
+import play.sbt.PlayImport._
+import sbt.Keys.libraryDependencies
+import sbt._
+
+object AppDependencies {
+
+  val compile = Seq(
+    ws,
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-27" % "3.2.0",
+    "uk.gov.hmrc"             %% "govuk-template"             % "5.61.0-play-27",
+    "uk.gov.hmrc"             %% "play-ui"                    % "8.20.0-play-27",
+    "uk.gov.hmrc"             %% "play-frontend-govuk"        % "0.57.0-play-27",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "0.35.0-play-27",
+    "org.webjars.npm"         %  "govuk-frontend"             % "3.10.2",
+    "org.webjars.npm"         %  "hmrc-frontend"              % "1.23.1"
+  )
+
+  val test = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "2.23.0"                % Test,
+    "org.scalatest"           %% "scalatest"                % "3.1.2"                 % Test,
+    "org.jsoup"               %  "jsoup"                    % "1.10.2"                % Test,
+    "com.typesafe.play"       %% "play-test"                % current                 % Test,
+    "org.mockito"             %  "mockito-core"             % "3.5.7"                 % Test,
+    "org.scalatestplus"       %% "scalatestplus-mockito"    % "1.0.0-M2"              % Test,
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10"               % "test, it",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"                 % "test, it",
+    "com.github.tomakehurst"  %  "wiremock-jre8"            % "2.27.2"                % "test, it"
+  )
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,24 @@
+resolvers += Resolver.url("HMRC Sbt Plugin Releases", url("https://dl.bintray.com/hmrc/sbt-plugin-releases"))(
+  Resolver.ivyStylePatterns)
+
+resolvers += "HMRC Releases" at "https://dl.bintray.com/hmrc/releases"
+
+resolvers += Resolver.typesafeRepo("releases")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-web" % "1.4.3")
+
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "2.13.0")
+
+addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "2.1.0")
+
+addSbtPlugin("org.irundaia.sbt" % "sbt-sassify" % "1.5.1")
+
+addSbtPlugin("uk.gov.hmrc" % "sbt-artifactory" % "1.3.0")
+
+addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.0.0")
+
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.7.5")
+
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.0")
+
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.6.1")
