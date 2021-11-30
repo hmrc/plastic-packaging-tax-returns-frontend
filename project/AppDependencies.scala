40ecc2431ab88c2f52234f09a5996a361a217de9
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2021-11-30 11:35:51 +0000

    BAU - update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   val compile = Seq(
     ws,
     "uk.gov.hmrc"                     %% "bootstrap-frontend-play-28" % "5.16.0",
-    "uk.gov.hmrc"                     %% "play-frontend-hmrc"         % "1.21.0-play-28"
+    "uk.gov.hmrc"                     %% "play-frontend-hmrc"         % "1.26.0-play-28"
   )
 
   val test = Seq(
