122dae349d993b4334a7b80e3d21e8ed064b011e
carl.thompson <carl.thompson@opencastsoftware.com>
2022-04-06 11:01:13 +0100

    Error messages updated and areYouSure No point to viewReturnSum

--- app/navigation/Navigator.scala
+++ app/navigation/Navigator.scala
@@ -47,7 +47,7 @@ class Navigator @Inject() () {
   private def amendAreYouSureRoute(answers: UserAnswers): Call =
     answers.get(AmendAreYouSurePage) match {
       case Some(true)  => routes.AmendManufacturedPlasticPackagingController.onPageLoad(NormalMode)
-      case Some(false) => routes.IndexController.onPageLoad
+      case Some(false) => routes.ViewReturnSummaryController.onPageLoad
       case None        => throw new Exception("Unable to navigate to page")
     }
 
