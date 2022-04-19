611ea780b9ea6ba66ae4271e35914ed68b2868ce
Liam Oldfield <liamoldfield@hotmail.co.uk>
2022-04-19 10:23:08 +0100

    refatcor

--- app/controllers/helpers/TaxReturnHelper.scala
+++ app/controllers/helpers/TaxReturnHelper.scala
@@ -37,7 +37,7 @@ class TaxReturnHelper @Inject() (
     extends FrontendBaseController with I18nSupport {
 
   // TODO - where do we get this obligation from? A GET on the return?
-  val defaultObligation: TaxReturnObligation = TaxReturnObligation(
+  private val defaultObligation: TaxReturnObligation = TaxReturnObligation(
     fromDate = LocalDate.parse("2022-04-01"),
     toDate = LocalDate.parse("2022-06-30"),
     dueDate = LocalDate.parse("2022-09-30"),
