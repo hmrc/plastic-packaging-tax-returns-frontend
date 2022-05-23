1a6e3f80a2118a64273b463af258ef761c1ee2d1
carl.thompson <carl.thompson@opencastsoftware.com>
2022-05-23 13:38:15 +0100

    long page templates added

---
+++ .g8/longPage/app/forms/$className$FormProvider.scala
@@ -0,0 +1,17 @@
+package forms
+
+import forms.mappings.Mappings
+import javax.inject.Inject
+import play.api.data.Form
+
+class $className$FormProvider @Inject() extends Mappings {
+
+  def apply(): Form[Long] =
+    Form(
+      "value" -> long(
+        "$className;format="decap"$.error.required",
+        "$className;format="decap"$.error.wholeNumber",
+        "$className;format="decap"$.error.nonNumeric")
+          .verifying(inRange($minimum$, $maximum$, "$className;format="decap"$.error.outOfRange"""))
+    )
+}
