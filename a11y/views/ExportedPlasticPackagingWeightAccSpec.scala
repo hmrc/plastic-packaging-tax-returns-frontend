be716d0129596d88252ed59ce92329b14f503bb8
mauro-fanton <mauro.fanton@opencastsoftware.com>
2022-07-18 09:54:41 +0100

    resolving some comments

--- a11y/views/ExportedPlasticPackagingWeightAccSpec.scala
+++ a11y/views/ExportedPlasticPackagingWeightAccSpec.scala
@@ -16,22 +16,6 @@
 
 package views
 
-/*
- * Copyright 2022 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import base.ViewSpecBase
 import forms.returns.ExportedPlasticPackagingWeightFormProvider
 import models.Mode.NormalMode
@@ -50,7 +34,7 @@ class ExportedPlasticPackagingWeightAccSpec
   private def createView: Html =
     page(form, NormalMode, 1234L)(request, messages)
 
-  "returns.ExportedPlasticPackagingWeightAccSpec" should {
+  "ExportedPlasticPackagingWeightView" should {
 
     "pass accessibility checks" in {
       createView.toString() must passAccessibilityChecks
