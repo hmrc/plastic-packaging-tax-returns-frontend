a888f489acb8575963753751a82d2f3da7649d3c
mauro-fanton <mauro.fanton@opencastsoftware.com>
2022-07-21 11:22:06 +0100

    refactor

--- app/navigation/ReturnsJourneyNavigator.scala
+++ app/navigation/ReturnsJourneyNavigator.scala
@@ -77,17 +77,12 @@ class ReturnsJourneyNavigator {
     }
 
   def manufacturedPlasticPackagingRoute(mode: Mode, hasAnswerChanged: Boolean, usersAnswer: Boolean): Call = {
-    if(mode == NormalMode && !hasAnswerChanged && usersAnswer) routes.ManufacturedPlasticPackagingWeightController.onPageLoad(mode)
-    else if(mode == NormalMode && !hasAnswerChanged && !usersAnswer) routes.ImportedPlasticPackagingController.onPageLoad(mode)
-    else
-    if (hasAnswerChanged) {
       (mode, usersAnswer) match {
-        case (_, true)       => routes.ManufacturedPlasticPackagingWeightController.onPageLoad(mode)
-        case (CheckMode, _)  => routes.ConfirmPlasticPackagingTotalController.onPageLoad
-        case (NormalMode, _) => routes.ImportedPlasticPackagingController.onPageLoad(mode)
+        case(NormalMode, true) if !hasAnswerChanged => routes.ManufacturedPlasticPackagingWeightController.onPageLoad(mode)
+        case (NormalMode, false) => routes.ImportedPlasticPackagingController.onPageLoad(mode)
+        case (_, true)  if  hasAnswerChanged   => routes.ManufacturedPlasticPackagingWeightController.onPageLoad(mode)
+        case _  => routes.ConfirmPlasticPackagingTotalController.onPageLoad
       }
-    } else
-      routes.ConfirmPlasticPackagingTotalController.onPageLoad
   }
 
   private def manufacturedPlasticPackagingWeightRoute(answers: UserAnswers): Call =
