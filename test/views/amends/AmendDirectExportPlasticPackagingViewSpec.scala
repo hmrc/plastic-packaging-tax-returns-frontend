ff2a197ffad2ae7a104836a4f9fcaa302f28be87
carl.thompson <carl.thompson@opencastsoftware.com>
2022-08-09 16:41:00 +0100

    View specs added for amends pages

---
+++ test/views/amends/AmendDirectExportPlasticPackagingViewSpec.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.amends
+
+import base.ViewSpecBase
+import forms.amends.AmendDirectExportPlasticPackagingFormProvider
+import models.returns.TaxReturnObligation
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.data.Form
+import play.twirl.api.Html
+import views.html.amends.AmendDirectExportPlasticPackagingView
+
+import java.time.LocalDate
+
+class AmendDirectExportPlasticPackagingViewSpec extends ViewSpecBase {
+
+  val page: AmendDirectExportPlasticPackagingView = inject[AmendDirectExportPlasticPackagingView]
+  val form: Form[Long] = new AmendDirectExportPlasticPackagingFormProvider()()
+
+  val aTaxObligation: TaxReturnObligation = TaxReturnObligation(LocalDate.now(), LocalDate.now().plusWeeks(12), LocalDate.now().plusWeeks(16), "PK1")
+
+  private def createView(form: Form[Long] = form): Html =
+    page(form, aTaxObligation)(request, messages)
+
+  "Amend Direct Export packaging page" should {
+
+    "have a paragraph" in {
+      val view: Html = createView()
+      val doc: Document = Jsoup.parse(view.toString())
+
+      doc.text() must include(messages("amendDirectExportPlasticPackaging.para"))
+    }
+
+
+    "have a hint" in {
+      val view: Html = createView()
+      val doc: Document = Jsoup.parse(view.toString())
+
+      doc.getElementById("value-hint").text must include(messages("amendDirectExportPlasticPackaging.hint"))
+    }
+
+    "display error" when {
+      "negative number submitted" in {
+        val view: Html = createView(form.fillAndValidate(-1))
+        val doc: Document = Jsoup.parse(view.toString())
+
+        doc.text() must include(messages("amendDirectExportPlasticPackaging.error.outOfRange.low"))
+      }
+
+      "number submitted is greater than maximum" in {
+        val view: Html = createView(form.fillAndValidate(999999999999L))
+        val doc: Document = Jsoup.parse(view.toString())
+
+        doc.text() must include(messages("amendDirectExportPlasticPackaging.error.outOfRange.high"))
+      }
+
+    }
+
+  }
+}
---
+++ test/views/amends/AmendRecycledPlasticPackagingViewSpec.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.amends
+
+import base.ViewSpecBase
+import forms.amends.AmendRecycledPlasticPackagingFormProvider
+import models.returns.TaxReturnObligation
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.data.Form
+import play.twirl.api.Html
+import views.html.amends.AmendRecycledPlasticPackagingView
+
+import java.time.LocalDate
+
+class AmendRecycledPlasticPackagingViewSpec extends ViewSpecBase {
+
+  val page: AmendRecycledPlasticPackagingView = inject[AmendRecycledPlasticPackagingView]
+  val form: Form[Long] = new AmendRecycledPlasticPackagingFormProvider()()
+
+  val aTaxObligation: TaxReturnObligation = TaxReturnObligation(LocalDate.now(), LocalDate.now().plusWeeks(12), LocalDate.now().plusWeeks(16), "PK1")
+
+  private def createView(form: Form[Long] = form): Html =
+    page(form, aTaxObligation)(request, messages)
+
+  "Amend Recycled packaging page" should {
+
+    "have a paragraph" in {
+      val view: Html = createView()
+      val doc: Document = Jsoup.parse(view.toString())
+
+      doc.text() must include(messages("amendRecycledPlasticPackaging.para"))
+    }
+
+
+    "have a hint" in {
+      val view: Html = createView()
+      val doc: Document = Jsoup.parse(view.toString())
+
+      doc.getElementById("value-hint").text must include(messages("amendRecycledPlasticPackaging.hint"))
+    }
+
+    "display error" when {
+      "negative number submitted" in {
+        val view: Html = createView(form.fillAndValidate(-1))
+        val doc: Document = Jsoup.parse(view.toString())
+
+        doc.text() must include(messages("amendRecycledPlasticPackaging.error.outOfRange.low"))
+      }
+
+      "number submitted is greater than maximum" in {
+        val view: Html = createView(form.fillAndValidate(999999999999L))
+        val doc: Document = Jsoup.parse(view.toString())
+
+        doc.text() must include(messages("amendRecycledPlasticPackaging.error.outOfRange.high"))
+      }
+
+    }
+
+  }
+}
+
