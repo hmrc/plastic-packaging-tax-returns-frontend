d81536223e841c43051fed89ffdf5f88254c3879
“lmcewanhmrc” <lindsay.mcewan@digital.hmrc.gov.uk>
2022-11-03 12:39:52 +0000

    More tests on SubscriptionService

--- app/services/SubscriptionService.scala
+++ app/services/SubscriptionService.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package services
 
 import connectors.SubscriptionConnector
@@ -13,7 +29,6 @@ class SubscriptionService @Inject() (
   subscriptionConnector: SubscriptionConnector
 ) (implicit ec: ExecutionContext) {
 
-
   def fetchGroupMemberNames(pptReferenceNumber: String) (implicit hc: HeaderCarrier): Future[GroupMembers] = {
     subscriptionConnector
       .get(pptReferenceNumber)
@@ -23,8 +38,8 @@ class SubscriptionService @Inject() (
   private def extractNames(connectorResponse: Either[EisFailure, SubscriptionDisplayResponse]): GroupMembers = {
     connectorResponse.fold(
       eisFailure => throw new RuntimeException("Subscription connector failed"),
-      subscriptionConnector => GroupMembers(Seq()))
-        
+      GroupMembers.create
+    )
   }
-  
+
 }
