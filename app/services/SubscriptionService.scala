4437d43f375ff83a25e26c3e6874d44a5404c258
“lmcewanhmrc” <lindsay.mcewan@digital.hmrc.gov.uk>
2022-11-03 12:09:32 +0000

    Adding SubscriptionService

--- app/services/SubscriptionService.scala
+++ app/services/SubscriptionService.scala
@@ -1,16 +1,30 @@
 package services
 
 import connectors.SubscriptionConnector
+import models.EisFailure
+import models.subscription.GroupMembers
+import models.subscription.subscriptionDisplay.SubscriptionDisplayResponse
 import uk.gov.hmrc.http.HeaderCarrier
 
 import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
 
 class SubscriptionService @Inject() (
   subscriptionConnector: SubscriptionConnector
-) {
+) (implicit ec: ExecutionContext) {
 
-  def fetchGroupMemberNames(pptReferenceNumber: String) (implicit hc: HeaderCarrier) = {
-    subscriptionConnector.get(pptReferenceNumber)
+
+  def fetchGroupMemberNames(pptReferenceNumber: String) (implicit hc: HeaderCarrier): Future[GroupMembers] = {
+    subscriptionConnector
+      .get(pptReferenceNumber)
+      .map(extractNames)
+  }
+
+  private def extractNames(connectorResponse: Either[EisFailure, SubscriptionDisplayResponse]): GroupMembers = {
+    connectorResponse.fold(
+      eisFailure => throw new RuntimeException("Subscription connector failed"),
+      subscriptionConnector => GroupMembers(Seq()))
+        
   }
   
 }
--- test/services/SubscriptionServiceSpec.scala
+++ test/services/SubscriptionServiceSpec.scala
@@ -1,30 +1,45 @@
 package services
 
 import connectors.SubscriptionConnector
+import models.subscription.GroupMembers
+import models.subscription.subscriptionDisplay.SubscriptionDisplayResponse
 import org.mockito.ArgumentMatchersSugar.any
 import org.mockito.MockitoSugar
-import org.mockito.MockitoSugar.{mock, verify}
+import org.mockito.MockitoSugar.{mock, verify, when}
 import org.scalatest.BeforeAndAfterEach
 import org.scalatestplus.play.PlaySpec
+import play.api.test.Helpers.{await, defaultAwaitTimeout}
 import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 class SubscriptionServiceSpec extends PlaySpec with BeforeAndAfterEach {
 
   private val subscriptionConnector: SubscriptionConnector = mock[SubscriptionConnector]
   private val headerCarrier: HeaderCarrier = mock[HeaderCarrier]
+  private val subscriptionDisplayResponse: SubscriptionDisplayResponse = mock[SubscriptionDisplayResponse]
 
   private val service = new SubscriptionService(subscriptionConnector)
 
   override protected def beforeEach(): Unit = {
     super.beforeEach()
     MockitoSugar.reset(subscriptionConnector, headerCarrier)
+    when(subscriptionConnector.get(any)(any)) thenReturn Future.successful(Right(subscriptionDisplayResponse))
   }
 
+
   "SubscriptionService" should {
+    
     "call its connector" in {
       service.fetchGroupMemberNames("ppt-ref")(headerCarrier)    
       verify(subscriptionConnector).get("ppt-ref")(headerCarrier)
     }
+    
+    "extract the group member names" in {
+      await(service.fetchGroupMemberNames("ppt-ref")(headerCarrier)) mustBe GroupMembers(Seq())
+    }
+    
   }
 
 }
