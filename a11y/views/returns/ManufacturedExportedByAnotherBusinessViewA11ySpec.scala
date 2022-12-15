6c744583912f355f52b7af3267f70712ebceee59
mauro-fanton <mauro.fanton@opencastsoftware.com>
2022-12-15 16:48:00 +0000

    added accessibility tests

---
+++ a11y/views/returns/ManufacturedExportedByAnotherBusinessViewA11ySpec.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.returns
+
+import base.ViewSpecBase
+import forms.returns.ManufacturedExportedByAnotherBusinessFormProvider
+import models.Mode.NormalMode
+import play.api.data.Form
+import uk.gov.hmrc.scalatestaccessibilitylinter.AccessibilityMatchers
+import views.html.returns.ManufacturedExportedByAnotherBusinessView
+
+class ManufacturedExportedByAnotherBusinessViewA11ySpec extends ViewSpecBase with AccessibilityMatchers {
+
+  private val page = inject[ManufacturedExportedByAnotherBusinessView]
+  private val form = new ManufacturedExportedByAnotherBusinessFormProvider()()
+
+  private def render(form: Form[Boolean] = form): String =
+    page(form, NormalMode, 200L)(request, messages).toString()
+
+  "pass accessibility checks without error" in {
+    render() must passAccessibilityChecks
+  }
+
+  "pass accessibility checks with error" in {
+    render(form.withError("error", "error message")) must passAccessibilityChecks
+  }
+}
