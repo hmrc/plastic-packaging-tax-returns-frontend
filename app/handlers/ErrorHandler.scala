3afe6ae64bd7637470866059ad44d68fa894e91e
Liam Oldfield <liamoldfield@hotmail.co.uk>
2023-01-31 14:55:51 +0000

    Error handler to not alert for BE fails

--- app/handlers/ErrorHandler.scala
+++ app/handlers/ErrorHandler.scala
@@ -16,16 +16,33 @@
 
 package handlers
 
-import javax.inject.{Inject, Singleton}
+import connectors.DownstreamServiceError
+import play.api.Logging
+import play.api.http.HeaderNames.CACHE_CONTROL
 import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.mvc.Request
+import play.api.mvc.Results.InternalServerError
+import play.api.mvc.{Request, RequestHeader, Result}
 import play.twirl.api.Html
-import uk.gov.hmrc.play.bootstrap.frontend.http.FrontendErrorHandler
+import uk.gov.hmrc.play.bootstrap.frontend.http.{ApplicationException, FrontendErrorHandler}
 import views.html.ErrorTemplate
+
+import javax.inject.{Inject, Singleton}
+import scala.language.implicitConversions
 
 @Singleton
 class ErrorHandler @Inject() (val messagesApi: MessagesApi, view: ErrorTemplate)
-    extends FrontendErrorHandler with I18nSupport {
+    extends FrontendErrorHandler with I18nSupport with Logging {
+
+  private implicit def rhToRequest(rh: RequestHeader): Request[_] = Request(rh, "")
+
+  override def resolveError(rh: RequestHeader, ex: Throwable): Result = ex match {
+    case ApplicationException(result, _) => result
+    case _: DownstreamServiceError =>
+      InternalServerError(internalServerErrorTemplate(rh)).withHeaders(CACHE_CONTROL -> "no-cache")
+    case _ =>
+      logger.error("PPT_ERROR_RAISE_ALERT uncaught exception not from downstream", ex)
+      InternalServerError(internalServerErrorTemplate(rh)).withHeaders(CACHE_CONTROL -> "no-cache")
+  }
 
   override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit
     rh: Request[_]
