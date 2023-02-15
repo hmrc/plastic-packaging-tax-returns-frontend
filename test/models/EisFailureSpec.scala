9539844649a6494e94d2fa73fe5c960b481062fd
“lmcewanhmrc” <lindsay.mcewan@digital.hmrc.gov.uk>
2023-02-15 14:10:27 +0000

    Rejig tests a bit

--- test/models/EisFailureSpec.scala
+++ test/models/EisFailureSpec.scala
@@ -12,32 +12,28 @@ class EisFailureSpec extends PlaySpec with BeforeAndAfterEach {
     super.beforeEach()
   }
   
+  private def cookSingleFailureResponse(code: String, description: String) =
+    JsObject(Seq(
+      "failures" -> JsArray(Seq(
+        JsObject(Seq(
+          "code" -> JsString(code),
+          "reason" -> JsString(description)
+        ))
+      ))
+    ))
+    
   "it" should {
     
     "recognise a de-registered account" when {
       
       "account is apparently de-registered" in {
-        val actualNotFoundResponseFromIf = JsObject(Seq(
-          "failures" -> JsArray(Seq(
-            JsObject(Seq(
-              "code" -> JsString("NO_DATA_FOUND"),
-              "reason" -> JsString("The remote endpoint has indicated that the requested resource could not be found.")
-            ))
-          ))
-        ))
+        val actualNotFoundResponseFromIf = cookSingleFailureResponse("NO_DATA_FOUND", 
+          "The remote endpoint has indicated that the requested resource could not be found.") 
         actualNotFoundResponseFromIf.asOpt[EisFailure].value.isDeregistered mustBe true
       }
       
       "something else, not de-registration" in {
-        val actualNotFoundResponseFromIf = JsObject(Seq(
-          "failures" -> JsArray(Seq(
-            JsObject(Seq(
-              "code" -> JsString("SUMMIT_ELSE"),
-              "reason" -> JsString("Bang")
-            ))
-          ))
-        ))
-        actualNotFoundResponseFromIf.asOpt[EisFailure].value.isDeregistered mustBe false
+        cookSingleFailureResponse("SUMMIT_ELSE", "Bang").asOpt[EisFailure].value.isDeregistered mustBe false
       }
       
     }
@@ -45,46 +41,22 @@ class EisFailureSpec extends PlaySpec with BeforeAndAfterEach {
     "recognise a downstream outage" when {
       
       "it's bad gateway" in {
-        // failures:
-        //                      - code: BAD_GATEWAY
-        //                        reason: >-
-        //                          Dependent systems are currently not responding.
-        val takenFromEisApiDoc = JsObject(Seq(
-          "failures" -> JsArray(Seq(
-            JsObject(Seq(
-              "code" -> JsString("BAD_GATEWAY"),
-              "reason" -> JsString("Dependent systems are currently not responding.")
-            ))
-          ))
-        ))
-
+        val takenFromEisApiDoc = cookSingleFailureResponse("BAD_GATEWAY", 
+          "Dependent systems are currently not responding.")
         takenFromEisApiDoc.asOpt[EisFailure].value.isDependentSystemsNotResponding mustBe true
       }
       
       "it's service unavailable" in {
-        val takenFromEisApiDoc = JsObject(Seq(
-          "failures" -> JsArray(Seq(
-            JsObject(Seq(
-              "code" -> JsString("SERVICE_UNAVAILABLE"),
-              "reason" -> JsString("Dependent systems are currently not responding.")
-            ))
-          ))
-        ))
-
+        val takenFromEisApiDoc = cookSingleFailureResponse("SERVICE_UNAVAILABLE", 
+          "Dependent systems are currently not responding.")
         takenFromEisApiDoc.asOpt[EisFailure].value.isDependentSystemsNotResponding mustBe true
       }
       
       "it's something else" in {
-        val takenFromEisApiDoc = JsObject(Seq(
-          "failures" -> JsArray(Seq(
-            JsObject(Seq(
-              "code" -> JsString("SUMMIT_ELSE"),
-              "reason" -> JsString("Bang")
-            ))
-          ))
-        ))
-
-        takenFromEisApiDoc.asOpt[EisFailure].value.isDependentSystemsNotResponding mustBe false
+        cookSingleFailureResponse("SUMMIT_ELSE", "Bang")
+          .asOpt[EisFailure]
+          .value
+          .isDependentSystemsNotResponding mustBe false
       }
       
     }
