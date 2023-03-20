66e0d7b34298391279e6d9f47a39aca4f6a17104
“lmcewanhmrc” <lindsay.mcewan@digital.hmrc.gov.uk>
2023-03-20 10:15:20 +0000

    Test coverage for localDateTimeNow()

--- test/util/EdgeOfSystemSpec.scala
+++ test/util/EdgeOfSystemSpec.scala
@@ -14,39 +14,43 @@
  * limitations under the License.
  */
 
-package config
+package util
 
-import org.mockito.ArgumentMatchersSugar.any
+import config.FrontendAppConfig
 import org.mockito.MockitoSugar
 import org.mockito.integrations.scalatest.ResetMocksAfterEachTest
 import org.scalatest.BeforeAndAfterEach
 import org.scalatestplus.play.PlaySpec
-import play.api.Configuration
-import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
 import java.time.LocalDateTime
 
-class FrontendAppConfigSpec extends PlaySpec
+class EdgeOfSystemSpec extends PlaySpec
   with MockitoSugar
   with BeforeAndAfterEach
   with ResetMocksAfterEachTest {
 
-  private val configuration = mock[Configuration]
-  private val servicesConfig = mock[ServicesConfig]
+  private val frontendAppConfig = mock[FrontendAppConfig]
+  private val edgeOfSystem = new EdgeOfSystem(frontendAppConfig)
+  
 
-  val frontendAppConfig = new FrontendAppConfig(configuration, servicesConfig)
-
-  "overrideSystemDateTime" should {
-
-    "parse the override if it's there" in {
-      when(configuration.getOptional[String](any)(any)) thenReturn Some("2023-03-31T23:59:58")
-      frontendAppConfig.overrideSystemDateTime.value mustBe LocalDateTime.of(2023, 3, 31, 23, 59, 58)
+  "localDateTimeNow" should {
+    
+    "test" in {
+      Numeric.Implicits
     }
-
-    "return None if override not present" in {
-      when(configuration.getOptional[String](any)(any)) thenReturn None
-      frontendAppConfig.overrideSystemDateTime mustBe empty
+    
+    "use the override time if it's there" in {
+      val exampleDateTime = LocalDateTime.of(2023, 4, 1, 12, 11, 10)
+      when(frontendAppConfig.overrideSystemDateTime) thenReturn Some(exampleDateTime)
+      edgeOfSystem.localDateTimeNow mustBe exampleDateTime
     }
-
+    
+    "use the system time if override not present" ignore { // todo
+      when(frontendAppConfig.overrideSystemDateTime) thenReturn None
+      edgeOfSystem.localDateTimeNow mustBe (10 +- 1)
+    }
+    
   }
+  
 }
+
