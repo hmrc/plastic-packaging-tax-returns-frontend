35e9956f0a2e77551157a66761e4ca64ff166f98
“lmcewanhmrc” <lindsay.mcewan@digital.hmrc.gov.uk>
2023-04-03 14:00:02 +0100

    Converted credits weight - add form

---
+++ test/controllers/returns/credits/ConvertedCreditsWeightControllerSpec.scala
@@ -0,0 +1,11 @@
+package controllers.returns.credits;
+
+import org.mockito.MockitoSugar
+import org.mockito.integrations.scalatest.ResetMocksAfterEachTest
+import org.scalatest.BeforeAndAfterEach
+import org.scalatestplus.play.PlaySpec
+
+class ConvertedCreditsWeightControllerSpec extends PlaySpec 
+  with MockitoSugar with BeforeAndAfterEach with ResetMocksAfterEachTest {
+    "cheese" in {}
+}
