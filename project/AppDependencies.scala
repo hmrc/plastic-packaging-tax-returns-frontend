ad5d5fd9f270086bc5cb6149cc8ed2c966eb0d9a
mauro-fanton <mauro.fanton@opencastsoftware.com>
2023-04-28 17:08:03 +0100

    remove play-json-joda library which is not used

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,8 +9,7 @@ object AppDependencies {
                     "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "6.6.0-play-28",
                     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.12.0-play-28",
                     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % bootstrapVersion,
-                    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % "0.74.0",
-                    "com.typesafe.play" %% "play-json-joda"                % "2.10.0-RC7"
+                    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % "0.74.0"
   )
 
   val test = Seq("org.scalatest"          %% "scalatest"                   % "3.2.10",
