a2981adc733754e537f88591a6412aff423f64a3
Liam Oldfield <liamoldfield@hotmail.co.uk>
2023-06-09 11:18:57 +0100

    wip

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
   val bootstrapVersion = "7.13.0"
 
   val compile = Seq(play.sbt.PlayImport.ws,
-                    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.8.0-play-28",
+                    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.9.0-play-28",
                     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.12.0-play-28",
                     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % bootstrapVersion,
                     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % "0.74.0"
