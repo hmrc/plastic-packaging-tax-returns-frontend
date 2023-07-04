c67209fead535e71ab07f953429e48d0dd007e89
“lmcewanhmrc” <lindsay.mcewan@digital.hmrc.gov.uk>
2023-07-04 13:28:26 +0100

    Imported weight controller - tweak redirect if obligation is missing

--- app/controllers/returns/ImportedPlasticPackagingWeightController.scala
+++ app/controllers/returns/ImportedPlasticPackagingWeightController.scala
@@ -20,7 +20,7 @@ import cacheables.ReturnObligationCacheable
 import connectors.CacheConnector
 import controllers.actions._
 import forms.returns.ImportedPlasticPackagingWeightFormProvider
-import models.Mode
+import models.{Mode, ReturnsUserAnswers}
 import models.returns.TaxReturnObligation
 import navigation.Navigator
 import pages.returns.ImportedPlasticPackagingWeightPage
@@ -46,16 +46,11 @@ class ImportedPlasticPackagingWeightController @Inject() (
     extends FrontendBaseController with I18nSupport {
 
   def onPageLoad(mode: Mode): Action[AnyContent] =
-    (identify andThen getData andThen requireData).async {
+    (identify andThen getData andThen requireData) {
       implicit request =>
-        val preparedForm = request.userAnswers.get(ImportedPlasticPackagingWeightPage) match {
-          case None        => form()
-          case Some(value) => form().fill(value)
-        }
-
-        request.userAnswers.get[TaxReturnObligation](ReturnObligationCacheable) match {
-          case Some(obligation) => Future.successful(Ok(view(preparedForm, mode, obligation)))
-          case None             => Future.successful(Redirect(controllers.routes.IndexController.onPageLoad))
+        ReturnsUserAnswers.checkObligationSync(request) { obligation =>
+          val preparedForm = request.userAnswers.fill(ImportedPlasticPackagingWeightPage, form())
+          Ok(view(preparedForm, mode, obligation))
         }
     }
 
