3c70a4b010762c28ef742736d3e9d178fb50d84e
Pan <35451824+liampan@users.noreply.github.com>
2023-07-11 09:32:14 +0100

    Update app/controllers/returns/PlasticExportedByAnotherBusinessController.scala

--- app/controllers/returns/PlasticExportedByAnotherBusinessController.scala
+++ app/controllers/returns/PlasticExportedByAnotherBusinessController.scala
@@ -69,7 +69,7 @@ class PlasticExportedByAnotherBusinessController @Inject()(
             request.userAnswers
               .setOrFail(AnotherBusinessExportedPage, value)
               .save(cacheConnector.saveUserAnswerFunc(pptId))
-              .map(updatedAnswer => Redirect(returnsNavigator.exportedByAnotherBusinessRoute(value, mode)))
+              .map(_ => Redirect(returnsNavigator.exportedByAnotherBusinessRoute(value, mode)))
       )
   }
 }
