5685ba078d78d750fb3182519265f3f624e068be
Liam Oldfield <liamoldfield@hotmail.co.uk>
2023-07-12 10:58:14 +0100

    update dependancies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,14 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  val bootstrapVersion = "7.15.0"
+  val bootstrapVersion = "7.19.0"
+  val hmrcMongoVersion = "1.3.0"
 
   val compile = Seq(play.sbt.PlayImport.ws,
-                    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.9.0-play-28",
-                    "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.12.0-play-28",
+                    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.14.0-play-28",
+                    "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.13.0-play-28",
                     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % bootstrapVersion,
-                    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % "1.2.0"
+                    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % hmrcMongoVersion
   )
 
   val test = Seq("org.scalatest"          %% "scalatest"                   % "3.2.10",
@@ -21,7 +22,7 @@ object AppDependencies {
                  "com.typesafe.play"      %% "play-test"                   % PlayVersion.current,
                  "org.mockito"            %% "mockito-scala"               % "1.16.42",
                  "org.scalacheck"         %% "scalacheck"                  % "1.15.4",
-                 "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"     % "1.2.0",
+                 "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"     % hmrcMongoVersion,
                  "com.vladsch.flexmark"    % "flexmark-all"                % "0.62.2",
                  "com.github.tomakehurst"  % "wiremock-jre8"               % "2.26.3",
                  "uk.gov.hmrc"            %% "bootstrap-test-play-28"      % bootstrapVersion
