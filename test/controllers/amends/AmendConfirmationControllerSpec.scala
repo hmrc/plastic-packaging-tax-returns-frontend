5f8d32a8445389a348b8e0a0fe28befe99be2a79
Liam Oldfield <liamoldfield@hotmail.co.uk>
2023-07-17 17:36:32 +0100

    [info] All done. Coverage was stmt=[95.07%] branch=[90.65%]

---
+++ test/controllers/amends/AmendConfirmationControllerSpec.scala
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.amends
+
+import akka.stream.testkit.NoMaterializer
+import base.FakeIdentifierActionWithEnrolment
+import org.mockito.ArgumentMatchers.refEq
+import org.mockito.ArgumentMatchersSugar.any
+import org.mockito.MockitoSugar.{reset, verify, when}
+import org.scalatest.BeforeAndAfterEach
+import org.scalatestplus.mockito.MockitoSugar.mock
+import org.scalatestplus.play.PlaySpec
+import play.api.http.Status.OK
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{await, contentAsString, defaultAwaitTimeout, status, stubMessagesControllerComponents, stubPlayBodyParsers}
+import play.twirl.api.Html
+import repositories.SessionRepository
+import repositories.SessionRepository.Paths
+import views.html.amends.AmendConfirmation
+
+import scala.concurrent.ExecutionContext.global
+import scala.concurrent.Future
+
+class AmendConfirmationControllerSpec extends PlaySpec
+  with BeforeAndAfterEach{
+
+  private val sessionRepository = mock[SessionRepository]
+  private val view = mock[AmendConfirmation]
+
+  private val sut = new AmendConfirmationController(
+    stubMessagesControllerComponents(),
+    new FakeIdentifierActionWithEnrolment(stubPlayBodyParsers(NoMaterializer)),
+    sessionRepository,
+    view
+  )(global)
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+
+    reset(view, sessionRepository)
+
+    when(view.apply(any)(any, any)).thenReturn(Html("correct view"))
+  }
+
+  "onPageLoad" should {
+    "return 200" in {
+      when(sessionRepository.get[String](any, any)(any)).thenReturn(Future.successful(Some("charge-ref")))
+
+      val result = sut.onPageLoad()(FakeRequest())
+
+      status(result) mustEqual OK
+      contentAsString(result) mustBe "correct view"
+      verify(view).apply(refEq(Some("charge-ref")))(any, any)
+      verify(sessionRepository).get(refEq("SomeId-123"), refEq(Paths.AmendChargeRef))(any)
+    }
+
+    "error" in {
+      object TestEx extends Exception("boom")
+
+      when(sessionRepository.get[String](any, any)(any)).thenReturn(Future.failed(TestEx))
+
+      intercept[TestEx.type](await(sut.onPageLoad()(FakeRequest())))
+      verify(sessionRepository).get(refEq("SomeId-123"), refEq(Paths.AmendChargeRef))(any)
+    }
+  }
+
+
+}
---
+++ test/models/subscription/LegalEntityDetailsSpec.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.subscription
+
+import org.mockito.MockitoSugar.when
+import org.mockito.stubbing.ReturnsDeepStubs
+import org.scalatestplus.mockito.MockitoSugar.mock
+import org.scalatestplus.play.PlaySpec
+
+class LegalEntityDetailsSpec extends PlaySpec {
+
+  val mockCustomerDetails = mock[CustomerDetails](ReturnsDeepStubs)
+
+  val sut = LegalEntityDetails(
+    "date-of-application",
+    "customerIdentification1",
+    Some("customerIdentification2"),
+    customerDetails = mockCustomerDetails,
+    groupSubscriptionFlag = true,
+    regWithoutIDFlag = true,
+    partnershipSubscriptionFlag = true
+  )
+
+  "entityName" must {
+    "get the name" when {
+      "Individual" in {
+        when(mockCustomerDetails.customerType).thenReturn(CustomerType.Individual)
+        when(mockCustomerDetails.individualDetails.get.toDisplayString).thenReturn("Individual-entityName")
+
+        sut.entityName mustBe "Individual-entityName"
+      }
+      "Organisation" in {
+        when(mockCustomerDetails.customerType).thenReturn(CustomerType.Organisation)
+        when(mockCustomerDetails.organisationDetails.get.organisationName).thenReturn("Organisation-entityName")
+
+        sut.entityName mustBe "Organisation-entityName"
+      }
+    }
+  }
+
+  "isGroup" must {
+    "proxy to groupSubscriptionFlag" in {
+      sut.isGroup mustBe true
+      sut.copy(groupSubscriptionFlag = false).isGroup mustBe false
+    }
+  }
+
+  "isPartnership" must {
+    "proxy to partnershipSubscriptionFlag" in {
+      sut.isPartnership mustBe true
+      sut.copy(partnershipSubscriptionFlag = false).isPartnership mustBe false
+    }
+  }
+
+}
