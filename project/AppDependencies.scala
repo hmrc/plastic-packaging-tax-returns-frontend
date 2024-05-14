f58c9d9ccefa23816eb8a028b42e4fea1462ae91
Chandra Andem <36305926+chandraAndem@users.noreply.github.com>
2024-05-14 18:02:32 +0100

    Dependency upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,21 +3,21 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  val bootstrapVersion = "8.3.0"
-  val hmrcMongoVersion = "1.8.0"
+  val bootstrapVersion = "8.6.0"
+  val hmrcMongoVersion = "1.9.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "8.5.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "9.10.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "2.0.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion
   )
 
   val test = Seq(
-    "org.scalatest"     %% "scalatest"               % "3.2.16",
+    "org.scalatest"     %% "scalatest"               % "3.2.18",
     "org.scalatestplus" %% "scalacheck-1-15"         % "3.2.11.0",
-    "org.mockito"       %% "mockito-scala-scalatest" % "1.17.14",
+    "org.mockito"       %% "mockito-scala-scalatest" % "1.17.31",
     "org.playframework" %% "play-test"               % PlayVersion.current,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion
