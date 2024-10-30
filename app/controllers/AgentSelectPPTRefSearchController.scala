b9387a0cda5aa93397b2a025b6e991955a59e4f1
Ming Sun <86245839+sunmingdh@users.noreply.github.com>
2024-10-30 12:56:16 +0000

    DDCYLS-5731: Replace client reference from MDTP session with mongoDB session (#614)

---
+++ app/controllers/AgentSelectPPTRefSearchController.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import play.api.i18n.MessagesApi
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
+import repositories.SessionRepository
+import repositories.SessionRepository.Paths.AgentSelectedPPTRef
+import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals.internalId
+import uk.gov.hmrc.auth.core.{AffinityGroup, AuthConnector, AuthorisedFunctions}
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import uk.gov.hmrc.play.http.HeaderCarrierConverter
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class AgentSelectPPTRefSearchController @Inject() (
+  val authConnector: AuthConnector,
+  override val messagesApi: MessagesApi,
+  override val controllerComponents: MessagesControllerComponents,
+  sessionRepository: SessionRepository
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with AuthorisedFunctions {
+
+  def get(): Action[AnyContent] = Action.async { implicit request =>
+    implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequest(request)
+
+    authorised(AffinityGroup.Agent).retrieve(internalId) { maybeInternalId =>
+      maybeInternalId.fold(Future.successful[Result](NotFound))(internalId =>
+        sessionRepository.get[String](internalId, AgentSelectedPPTRef).map {
+          case Some(registration) => Ok(Json.obj("clientPPT" -> registration))
+          case None               => NotFound
+        }
+      )
+    }
+  }
+}
---
+++ test/controllers/AgentSelectPPTRefSearchControllerSpec.scala
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import forms.AgentsFormProvider
+import org.mockito.ArgumentMatchers.any
+import org.mockito.MockitoSugar.{reset, when}
+import org.scalatest.BeforeAndAfterEach
+import org.scalatestplus.mockito.MockitoSugar.mock
+import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.Json
+import play.api.mvc.Result
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import repositories.SessionRepository
+import uk.gov.hmrc.auth.core.AuthConnector
+
+import scala.concurrent.ExecutionContext.global
+import scala.concurrent.Future
+
+class AgentSelectPPTRefSearchControllerSpec extends PlaySpec with BeforeAndAfterEach {
+
+  val formProvider = mock[AgentsFormProvider]
+
+  val mockAuthConnector     = mock[AuthConnector]
+  val mockSessionRepository = mock[SessionRepository]
+
+  val sut = new AgentSelectPPTRefSearchController(
+    mockAuthConnector,
+    stubMessagesControllerComponents().messagesApi,
+    stubMessagesControllerComponents(),
+    mockSessionRepository
+  )(global)
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    reset(mockAuthConnector, mockSessionRepository)
+  }
+
+  val validInternalId   = "Int-123"
+  val validPPTReference = "123456"
+
+  "get" must {
+    "return valid PPT reference" in {
+      when(mockAuthConnector.authorise[Option[String]](any(), any())(any(), any()))
+        .thenReturn(Future.successful(Some(validInternalId)))
+      when(mockSessionRepository.get[String](any(), any())(any())).thenReturn(
+        Future.successful(Some(validPPTReference))
+      )
+      val result: Future[Result] = sut.get()(FakeRequest())
+      status(result) mustBe OK
+      contentAsJson(result) mustBe Json.obj("clientPPT" -> validPPTReference)
+    }
+
+    "error" when {
+      "InternalId does not exist" in {
+        when(mockAuthConnector.authorise[Option[String]](any(), any())(any(), any()))
+          .thenReturn(Future.successful(None))
+        val result: Future[Result] = sut.get()(FakeRequest())
+        status(result) mustBe NOT_FOUND
+      }
+
+      "PPT reference does not exist" in {
+        when(mockAuthConnector.authorise[Option[String]](any(), any())(any(), any()))
+          .thenReturn(Future.successful(Some(validInternalId)))
+        when(mockSessionRepository.get[String](any(), any())(any())).thenReturn(Future.successful(None))
+        val result: Future[Result] = sut.get()(FakeRequest())
+        status(result) mustBe NOT_FOUND
+      }
+
+      "auth fails" in {
+        object TestException extends Exception("test")
+        when(mockAuthConnector.authorise[Unit](any(), any())(any(), any()))
+          .thenReturn(Future.failed(TestException))
+
+        intercept[TestException.type](await(sut.get()(FakeRequest())))
+      }
+    }
+  }
+
+}
