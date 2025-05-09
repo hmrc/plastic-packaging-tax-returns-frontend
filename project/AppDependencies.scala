adb89c3bf5ac0ee3b88c5ee1a2ec81c1a2f9a732
accenture-dami <203887119+accenture-dami@users.noreply.github.com>
2025-05-09 13:21:23 +0100

    Undo bootstrap frontend play upgrade, to be done later

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  val bootstrapVersion = "9.11.0"
+  val bootstrapVersion = "8.6.0"
   val hmrcMongoVersion = "2.6.0"
 
   val compile = Seq(
